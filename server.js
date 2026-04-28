4425e69008105a94c28dc22269ad303381980f68
MuhammadAsadkhan-11 <asadkhans2310861@gmail.com>
2026-04-28 11:46:00 +0500

    updated gemini key and model

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post("/chat", async (req, res) => {
     }
 
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [
           {
