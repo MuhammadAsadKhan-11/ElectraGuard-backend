94b94949ae9d61c5b7b6db6b018117b952fe43e4
MuhammadAsadkhan-11 <asadkhans2310861@gmail.com>
2026-05-08 16:12:53 +0500

    model version updated

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post("/chat", async (req, res) => {
 
     //  v1beta ki jagah v1 use karo, aur updated model name
     const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-3-flash-preview:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [{ parts: [{ text: message }] }],
       },
